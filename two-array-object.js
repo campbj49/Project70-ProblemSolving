408fbd58c98eeb0dceb20c29ea9d7abad0acf171
John Campbell <campbj49@gmail.com>
2023-09-29 13:01:22 -0400

    two-array-object completed

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,8 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+//makes an object out of the given keys and values
+function twoArrayObject(keys, values) {
+    let constructedObj = {};
+    for(i=0; i<keys.length; i++)
+        constructedObj[keys[i]] = values[i] || null;
+    return constructedObj;
 }
