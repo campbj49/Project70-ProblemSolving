a59b7b96bb91e631f346110624b2631ad8beabcd
John Campbell <campbj49@gmail.com>
2023-09-29 12:45:29 -0400

    is-subsequence complete

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,11 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+//checks if the substring exists as a set of characters in the same order in superstring
+function isSubsequence(sub, str) {
+    //loop through the superstring comparing consecutive characters of the substring
+    let subChr = 0;
+    for(let strChr of str){
+        if(strChr === sub[subChr]) subChr++;
+        if(subChr === sub.length) return true;
+    }
+    return false;
 }
