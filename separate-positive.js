8cc90506ad368c11a3779b2a8440dda683675ae2
John Campbell <campbj49@gmail.com>
2023-09-29 12:58:11 -0400

    seperate positives working

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+//takes an array and pushes the positive numbers to the left and negative to right
+function separatePositive(array) {
+    //can just tweak the pivot function from quicksort
+    let pivot = 0;
+    let pivotIndex = 0;
+    for(i=0; i<array.length; i++){
+        if(pivot<array[i]){
+            [array[i], array[pivotIndex]] = [array[pivotIndex],array[i]]
+            pivotIndex++;
+        }
+    }
+    return array;
 }
