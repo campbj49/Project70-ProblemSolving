1766082c87f562b40ab47fa6f4df3d028a99e990
John Campbell <campbj49@gmail.com>
2023-09-29 11:45:40 -0400

    average pair functioning

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+/**averagePair(array, target ) => pairExits
+ * Params:
+ * - array: array of numbers
+ * - target: single number
+ * 
+ * returns:
+ * pairExists: true or false depending on whether or not any pair of numbers in 
+ * array average to target
+ */
+function averagePair(array, target) {
+    //start with a naive solution of the double for loop
+    let start = 1;
+    for(let num of array){
+        for(i=start; i<array.length; i++){
+            if(num+array[i]===target*2) return true;
+        }
+        start++;
+    }
+    return false;
+
+    //theres almost certainly 
 }
