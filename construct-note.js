852c843c5b3db719d2b298985bb413b8e85925bb
John Campbell <campbj49@gmail.com>
2023-09-29 12:15:00 -0400

    construct-note functioning

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+//determines if the message can be made out of the chars
+function constructNote(message, chars) {
+    if(chars.length ===0) return false;
+    //get the character counts of each parameter
+    let msgCount = createCharCount(message);
+    let chrCount = createCharCount(chars);
+
+    //ensure that chrCount has at least as many of each character as msgCount
+    for(let key in msgCount)
+        if(msgCount[key] > chrCount[key]) return false;
+    return true;
+}
+//helper function that creates an object with the char count of a string
+function createCharCount(str){
+    let chrCount = {};
+    for(let chr of str){
+        if(chrCount[chr])chrCount[chr]++;
+        else chrCount[chr] = 1;
+    }
+    return chrCount;
 }
